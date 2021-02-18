ffd69815117949b30b5252b567b6ed0ff5623378
Bahram Jalili <bahram.jalili@CGIAdmins-MacBook-Pro.local>
2021-02-18 16:53:18 -0500

    Secure the code for public repository

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -1,6 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 2.70"
+    }
+  }
+}
 provider "aws" {
   region = var.aws_region
-  version = "~> 2.64"
 }
 
 locals {
