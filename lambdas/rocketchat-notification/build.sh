41de9c04fb408b6e71cb3e8a786c4cc332042f27
prabhukiran9999 <70853875+prabhukiran9999@users.noreply.github.com>
2021-08-16 10:26:03 -0400

    Update build.sh

--- lambdas/rocketchat-notification/build.sh
+++ lambdas/rocketchat-notification/build.sh
@@ -2,16 +2,15 @@
 pwd
 mkdir ../../builds
 chmod 777 ../../builds
-cat src/index.py  > lambda_function.py
+cat ../../lambdas/rocketchat-notification/src/index.py  > index.py
 pip3 install --target ./package requests
-cp lambda_function.py ./package
+cp index.py ./package
 cd package
 zip -r securityhubfindings-to-rocketchat.zip .
 cd ../../..
 pwd
-chmod -R 777 /Users/prabhu.manchineella/Desktop/ROCKETCHAT-NOTIFICATIONS/terraform-octk-aws-sea-rocketchat-notifications
-cd /Users/prabhu.manchineella/Desktop/ROCKETCHAT-NOTIFICATIONS/terraform-octk-aws-sea-rocketchat-notifications/lambdas/rocketchat-notification/package
-pwd
-cp -f securityhubfindings-to-rocketchat.zip /Users/prabhu.manchineella/Desktop/ROCKETCHAT-NOTIFICATIONS/terraform-octk-aws-sea-rocketchat-notifications/builds/securityhubfindings-to-rocketchat.zip
+chmod -R 777 /Users/prabhu.manchineella/Desktop/python_deployment/terraform-octk-aws-sea-rocketchat-notifications
+cd /Users/prabhu.manchineella/Desktop/python_deployment/terraform-octk-aws-sea-rocketchat-notifications/terraform/aws/package
+cp -f securityhubfindings-to-rocketchat.zip /Users/prabhu.manchineella/Desktop/python_deployment/terraform-octk-aws-sea-rocketchat-notifications/builds/securityhubfindings-to-rocketchat.zip
 
 
